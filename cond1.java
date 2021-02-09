f1d30b23d31f1b8f1f90365b839794a08e7bb6b5
Junior <74092633+juniorstz@users.noreply.github.com>
2021-02-09 18:58:50 -0300

    SALARY

---
+++ cond1.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class cond1 {
+	//Salary
+	public static  void main(String[] args) {
+		Locale.setDefault(Locale.US);	
+	 Scanner sc = new Scanner(System.in);
+
+
+	 
+	double A = sc.nextDouble();
+	double B = sc.nextInt();
+	 double C = sc.nextFloat();
+
+	 double salary = B * C;
+
+	System.out.println("NUMBER = " + A);
+	System.out.println("SALARY = " + salary);
+
+	sc.close();
+	}
+}
