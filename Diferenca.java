c178928c729844794ffebbadc88de98daf718854
Junior <74092633+juniorstz@users.noreply.github.com>
2021-02-09 19:05:07 -0300

    Diferenca

---
+++ Diferenca.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Diferenca {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);	
+		 Scanner sc = new Scanner(System.in);
+
+		 
+  double A = sc.nextInt();
+  double B = sc.nextInt();
+  double C = sc.nextInt();
+  double D = sc.nextInt();
+		 
+    double num = A * B;
+    double num2 = C * D;
+    double valor = num - num2;
+		  
+		  
+		  
+		  
+		  
+		  
+		  System.out.println("DIFERENÇA = " + valor);
+	}
+
+}
